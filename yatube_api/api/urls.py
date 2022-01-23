57b898f7907e91536ec0b1be03255d553cf9c50f
Offlinexaa <Offlinexaa@gmail.com>
2022-01-23 20:48:30 +0400

    Спринт 9. Добавлены вюсеты для: Group, Post.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from api.views import GroupViewSet, PostViewSet
+
+
+v1_router = SimpleRouter()
+v1_router.register('posts', PostViewSet)
+v1_router.register('groups', GroupViewSet)
 
 urlpatterns = [
+    path('', include(v1_router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,23 @@
-# TODO:  Напишите свой вариант
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework import permissions
+from rest_framework.pagination import LimitOffsetPagination
+
+from posts.models import Post, Group
+from api.serializers import PostSerializer, GroupSerializer
+from api.permissions import AuthorOrReadOnly
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
